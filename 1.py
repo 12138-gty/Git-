c96bc3793932ddca8c1243ac1fdabd7ae4c67c12
guitingyu1012 <gty17633210358@foxmail.com>
2025-07-19 09:55:55 +0800

    1.3

--- 1.py
+++ 1.py
@@ -1,1 +1,15 @@
 print("Hello, World!")
+# This is a simple Python script that prints a greeting message.
+# You can run this script using Python interpreter.
+a = 5
+b = 10
+print(f"Initial values: a = {a}, b = {b}")
+def exchange_values(x, y):
+    """Function to exchange values of two variables."""
+    return y, x
+# Exchanging values of a and b
+a, b = exchange_values(a, b)
+# The values of a and b are now exchanged.
+# After exchanging, the values of a and b will be swapped.
+print(f"Exchanged values: a = {a}, b = {b}")
+# The values of a and b have been exchanged successfully.
