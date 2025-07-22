9ceaacd1fdd70c3f8319243c018a0ff0954383fd
guitingyu1012 <gty17633210358@foxmail.com>
2025-07-22 08:46:29 +0800

    master分支已创建

--- 1.py
+++ 1.py
@@ -1,16 +1,1 @@
-b = 66
-c = 460
-e = 60
-f = 25*25
-g = 875
-h = b+c+e+f+g
-print(h)
-m = 660
-n = 1000
-p = 200
-print(m+n+p-h)
-print("hello world")
-print("hello world again")
-print("经过git add后，文件被添加到暂存区,再经过git reset HEAD <file>后，文件会从暂存区移除，回到工作区，这句话会保留")
-print("这还是branch主分支内容吗")
-print("experiment")
+print("master分支已建好")
